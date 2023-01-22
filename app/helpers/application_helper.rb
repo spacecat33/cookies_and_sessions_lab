4f19a74c6cd301e0e02e84c99d4bfe4259db79db
Janan Akkad <jananakkad@hotmail.co.uk>
2023-01-22 17:18:55 +0000

    Done - cookies and sessions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+    def current_cart
+      session[:cart] ||= []
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'products#index'
+  post '/products/add', to: 'products#add'
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+  root 'application#cart'
 end
